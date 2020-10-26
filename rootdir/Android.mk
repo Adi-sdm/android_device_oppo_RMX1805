a60c8b8b03e3d1995ebb019541d3c7d321064fb1
iamsaalim <saalimquadri1@gmail.com>
2020-10-26 11:46:06 +0000

    RMX1805: Build and bind mount fstab
    Change-Id: Ia6085be92851173ae7f105e1e87a0433be4789cf

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+<<<<<<< HEAD
 include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
@@ -96,12 +97,15 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab-wlan.qcom
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
+=======
+>>>>>>> 0f47117 (RMX1805: Build and bind mount fstab)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
+<<<<<<< HEAD
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
@@ -112,4 +116,15 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+=======
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+>>>>>>> 0f47117 (RMX1805: Build and bind mount fstab)
 include $(BUILD_PREBUILT)
