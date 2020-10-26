fd58af30e8e77bc12382b4a0731288678e4089ed
iamsaalim <saalimquadri1@gmail.com>
2020-10-26 11:31:22 +0000

    RMX1805: Build some radio dependant libs
    Change-Id: Ide93670234831ac63116528c7b1ecf21596f914b

--- device.mk
+++ device.mk
@@ -243,6 +243,10 @@ PRODUCT_BOOT_JARS += \
     WfdCommon
 
 # Inherit vendor
+# CNE
+PRODUCT_PACKAGES += \
+    libcnefeatureconfig
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
@@ -272,6 +276,16 @@ PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
     PresencePolling \
     RcsService
+
+# RIl
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0
+
+PRODUCT_PACKAGES += \
+    librmnetctl \
+    libxml2
 
 # Telephony
 PRODUCT_PACKAGES += \
