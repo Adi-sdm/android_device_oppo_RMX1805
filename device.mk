d7c26ae447fc92e7346fae88d8bf6075a000c921
baibhab <fakebai34@gmail.com>
2021-02-05 17:56:53 +0000

    RMX1805: add vendor signature

--- device.mk
+++ device.mk
@@ -37,8 +37,10 @@ PRODUCT_PACKAGES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
+# ifneq ($(findstring lineage, $(TARGET_PRODUCT)),)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+# endif
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
