c4362bfbaaecd4d7647c11f21d6ef57447548e15
Adi-sdm <adityabihat6@gmail.com>
2022-04-06 15:29:49 +0530

    RMX1805:Build audio.a2dp.default from source

--- device.mk
+++ device.mk
@@ -5,6 +5,11 @@
 #
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := RMX1805,RMX1811,RMX1809
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
